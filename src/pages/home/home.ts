b93d84eb68243cc09e90e2c0e4c329d222eb7878
Kathryn Sara <kathysara@Kathryns-MacBook-Air.local>
2017-04-05 12:03:18 +0930

    update for 2.3.0

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+  posts: any;
 
+  constructor(public navCtrl: NavController, public http: Http) {
+
+	this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+		this.posts = data.data.children;
+	});
+	
+  }
 }
